3653f5e689df0bed50dabfd27caa5cbb7dc1a615
AdamAod <adam@objecttechnica.io>
2021-12-21 18:17:59 -0800

    Delete modules directory

--- modules/kms/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "kms_key" {
-  description = "Output for the KMS key ID"
-  value       = aws_kms_key.stitchfix_key.arn
-}
--- modules/s3/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "bucket_name" {}
-variable "kms_key_id" {}
-variable "s3_acl" {}
-variable "force_destroy" {}
-variable "bucket_key" {}
-variable "master_id" {}
